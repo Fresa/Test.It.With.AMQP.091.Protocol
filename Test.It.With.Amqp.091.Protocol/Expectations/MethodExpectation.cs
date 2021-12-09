9f2a8f380d8aba62dfdbb36edf87c7262a794a72
Fredrik Arvidsson <fredrik@fkan.se>
2021-12-09 18:13:24 +0100

    refactor(expectation): add former received method which dictates what the next received methods should be

--- Test.It.With.Amqp.091.Protocol/Expectations/MethodExpectation.cs
+++ Test.It.With.Amqp.091.Protocol/Expectations/MethodExpectation.cs
@@ -5,9 +5,9 @@ namespace Test.It.With.Amqp091.Protocol.Expectations
 {
     internal class MethodExpectation : Expectation
     {
-        public MethodExpectation(params Type[] methods)
+        public MethodExpectation(Type method, params Type[] expectedMethods) : base(method)
         {
-            MethodResponses = methods.Distinct().ToArray();
+            MethodResponses = expectedMethods.Distinct().ToArray();
         }
 
         public Type[] MethodResponses { get; }
@@ -15,7 +15,12 @@ namespace Test.It.With.Amqp091.Protocol.Expectations
 
     internal class MethodExpectation<T> : MethodExpectation
     {
-        public MethodExpectation() : base(typeof(T))
+        public MethodExpectation() : base(null, typeof(T))
+        {
+
+        }
+
+        public MethodExpectation(Type method) : base(method, typeof(T))
         {
             
         }
